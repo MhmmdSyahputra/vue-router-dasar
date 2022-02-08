cdded98d2dfe541ebd0d43518a0434c8463ce561
MhmmdSyahputra <putra.cokers@gmail.com>
2022-02-08 14:45:53 +0700

    new

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import Home from '../components/Home.vue'
+import About from '../components/About.vue'
+import {
+    createRouter,
+    createWebHashHistory
+} from 'vue-router'
+
+const routes = [{
+        path: '/',
+        component: Home
+    },
+    {
+        path: '/about',
+        component: About
+    }
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+})
+
+export default router
